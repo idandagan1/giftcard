d290ac5a6f9d3d021bcec703f1f28e3813429fb2
Idan Dagan <idandagan1@gmail.com>
2017-10-03 23:30:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const routes = require('./server/routes');
 
-const mongodb = process.env.ENV === 'production' ? 'mongodb://emma:emmadagan@ds159274.mlab.com:59274/giftcard' : 'mongodb://localhost/giftcard';
+const mongodb = process.env.NODE_ENV === 'production' ? 'mongodb://emma:emmadagan@ds159274.mlab.com:59274/giftcard' : 'mongodb://localhost/giftcard';
 mongoose.connect(mongodb);
 
 app.use(bodyParser.urlencoded({
